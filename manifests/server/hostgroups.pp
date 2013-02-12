9f9de22d3550723cd3806455f98dee873f3acce8
francispereira <francispereira@7terminals.com>
2013-02-12 13:45:44 +0530

    added host group hostgroup-generic-servers

--- manifests/server/hostgroups.pp
+++ manifests/server/hostgroups.pp
@@ -1,8 +1,12 @@
 class nagios::server::hostgroups {
-	resources {
-		'nagios_hostgroup' :
-			name => 'nagios_hostgroup',
-			purge => true,
-	}
+  resources { 'nagios_hostgroup':
+    name  => 'nagios_hostgroup',
+    purge => true,
+  }
 
+  nagios_hostgroup { 'hostgroup-generic-servers':
+    ensure  => present,
+    alias   => 'Generic hostgroup',
+    members => 'localhost',
+  }
 }
