eea18188d224b8254413053a96131d49e5a4963e
francispereira <francispereira@7terminals.com>
2013-02-12 13:14:13 +0530

    collected all nagios exported resouces in server::core

--- manifests/server/core.pp
+++ manifests/server/core.pp
@@ -12,41 +12,62 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class nagios::server::core {
-	package {
-		'nagios' :
-			ensure => installed,
-	}
-	file {
-		'/etc/nagios/localhost.cfg' :
-			ensure => absent,
-			require => Package['nagios'],
-	}
-	file {
-		'/etc/nagios/commands.cfg' :
-			ensure => absent,
-			require => File['/etc/nagios/localhost.cfg'],
-	}
-	file {
-		'/etc/nagios/cgi.cfg' :
-			ensure => present,
-			source => 'puppet:///modules/nagios/server/cgi.cfg',
-			require => File['/etc/nagios/commands.cfg'],
-	}
-	file {
-		'/etc/nagios/nagios.cfg' :
-			ensure => present,
-			source => 'puppet:///modules/nagios/server/nagios.cfg',
-			require => File['/etc/nagios/cgi.cfg'],
-	}
-	file {
-		'/etc/nagios/passwd' :
-			ensure => present,
-			source => 'puppet:///modules/nagios/server/passwd',
-			require => File['/etc/nagios/nagios.cfg'],
-	}
-	service {
-		'nagios' :
-			ensure => running,
-			enable => true,
-	}
+  package { 'nagios': ensure => installed, }
+
+  file { '/etc/nagios/localhost.cfg':
+    ensure  => absent,
+    require => Package['nagios'],
+  }
+
+  file { '/etc/nagios/commands.cfg':
+    ensure  => absent,
+    require => File['/etc/nagios/localhost.cfg'],
+  }
+
+  file { '/etc/nagios/cgi.cfg':
+    ensure  => present,
+    source  => 'puppet:///modules/nagios/server/cgi.cfg',
+    require => File['/etc/nagios/commands.cfg'],
+  }
+
+  file { '/etc/nagios/nagios.cfg':
+    ensure  => present,
+    source  => 'puppet:///modules/nagios/server/nagios.cfg',
+    require => File['/etc/nagios/cgi.cfg'],
+  }
+
+  file { '/etc/nagios/passwd':
+    ensure  => present,
+    source  => 'puppet:///modules/nagios/server/passwd',
+    require => File['/etc/nagios/nagios.cfg'],
+  }
+
+  service { 'nagios':
+    ensure => running,
+    enable => true,
+  }
+  Nagios_command <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_contactgroup <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_contact <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_hostgroup <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_host <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_service <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_timeperiod <<| |>> {
+    notify => Service['nagios'],
+  }
+  Nagios_servicedependency <<| |>> {
+    notify => Service['nagios'],
+  }
 }
